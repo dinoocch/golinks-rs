642c68055bc69681b4699c048a59b5077cdb6220
dinoocch <dino.occhialini@gmail.com>
2017-08-07 00:56:00 -0700

    Add basic logging

--- README.md
+++ README.md
@@ -5,6 +5,21 @@ Simple service to manage url shortcuts.
 Primarily intended for personal use on a LAN,
 but *could* be external facing...
 
+Original concept credit to google.  There's lots of other adaptations out there.
+
+The concept is far too easy to get used to...plus I like these better than
+bookmarks.
+
+## Disclaimer
+
+This is primarily intended to be a quick introduction to the
+rust programming language and an easy Sunday project.
+
+Therefore there are certainly instances of things that
+could have been written better/more concisely.
+
+LMDB is used on a whim.
+
 ## Usage
 
 aliases map to endpoints:
@@ -13,6 +28,19 @@ go/google -> https://google.com
 GET requests to the alias return a redirect
 
 OPTIONS -> returns definition for alias
+
+PUT -> http://go/test with data "https://google.com"
+Replaces the alias test with the provided link.
+
+Bonus points:
+Use `{}` to accept arguments:
+  * Say `go/google => https://google.com/search?q={}`
+    Req `go/google/test` maps to `https://google.com/search?q=test`
+
+As many arguments are allowed.  Only {} will be replaced.
+Arguments that are not specified are left as {}...
+
+Future may make arguments optional ()
 
 ## Setup
 
@@ -28,4 +56,10 @@ hyper for HTTP
 
 lmdb for storage ^.^
 
-## Future work
+## What is not there (yet?)
+
+* Authentication (PUT/DELETE)
+* Web interface
+* High Availability/Scalability
+
+These features will likely never exist.
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,10 @@ extern crate lmdb;
 #[macro_use]
 extern crate lazy_static;
 
+#[macro_use]
+extern crate log;
+extern crate pretty_env_logger;
+
 use std::path::Path;
 use hyper::server::{Http, Request, Response, Service};
 use hyper::{Method, StatusCode, Chunk};
@@ -14,8 +18,10 @@ use lmdb::{Environment, WriteFlags, Transaction};
 
 lazy_static! {
     static ref DATABASE: Environment = {
+        info!("Initializing Database Environment");
         let dir_path = Path::new("/home/dino/go_linkdb");
         let db = Environment::new().open(dir_path).unwrap();
+        info!("[Finished] Initializing Database Environment");
         db
     };
 }
@@ -30,48 +36,167 @@ impl Service for GoLinkServer {
 
     fn call(&self, req: Request) -> Self::Future {
         let mut response = Response::new();
-        let db_handle = DATABASE.open_db(None).unwrap();
-        let (method, uri, _version, _headers, body) = req.deconstruct();
-        let path_str = uri.path().to_owned();
-        let path = &path_str[1..];
-        let path = path.to_owned();
-        match method {
-            Method::Get => {
-                futures::future::ok(1).and_then( move |_: u32| {
-                let txn = DATABASE.begin_ro_txn().unwrap();
-                let value = txn.get(db_handle, &path).unwrap();
-                let body = String::from_utf8(value.to_vec()).unwrap();
-                response.headers_mut().set(Location::new(body));
-                response.set_status(StatusCode::Found);
-                Ok(response)
-                }).boxed()
+        let db_handle = DATABASE.open_db(None);
+        match db_handle {
+            Ok(db_handle) => {
+                let (method, uri, _version, _headers, body) = req.deconstruct();
+                let path_str = uri.path().to_owned();
+                let path = &path_str[1..];
+                let path = path.to_owned();
+                debug!("Received new request for {}", path);
+                match method {
+                    Method::Get => {
+                        futures::future::ok(1).and_then( move |_: u32| {
+                            debug!("Opening Database Transaction (RO)");
+                            let txn = DATABASE.begin_ro_txn();
+                            match txn {
+                                Ok(txn) => {
+                                    let mut path_iter = path.split('/');
+                                    let value = match path_iter.next() {
+                                        Some(val) => {
+                                            txn.get(db_handle, &val)
+                                        },
+                                        None => { Err(lmdb::Error::Invalid) }
+                                    };
+                                    match value {
+                                        Ok(value) => {
+                                            let mut body = String::from_utf8(value.to_vec()).unwrap();
+                                            debug!("Got value {} for {}", body, path);
+                                            loop {
+                                                match path_iter.next() {
+                                                    Some(arg) => {
+                                                        body = body.replacen("{}", &arg, 1);
+                                                    },
+                                                    None => { break }
+                                                };
+                                            };
+                                            response.headers_mut().set(Location::new(body));
+                                            response.set_status(StatusCode::Found);
+                                        },
+                                        Err(_) => {
+                                            error!("Value not found: {}", path);
+                                            response.set_status(StatusCode::NotFound);
+                                        }
+                                    };
+                                },
+                                Err(_) => {
+                                    error!("Trouble opening database ro transaction");
+                                    response.set_status(StatusCode::InternalServerError);
+                                }
+                            };
+                            Ok(response)
+                        }).boxed()
+                    },
+                    Method::Put => {
+                        body.concat2().and_then(move |body_c: Chunk| {
+                            let txn = DATABASE.begin_rw_txn();
+                            match txn {
+                                Ok(mut txn) => {
+                                    let put = txn.put(db_handle, &path, &body_c, WriteFlags::empty());
+                                    match put {
+                                        Ok(_) => {
+                                            let commit = txn.commit();
+                                            match commit {
+                                                Ok(_) => {
+                                                    response.set_status(StatusCode::NoContent);
+                                                },
+                                                Err(_) => {
+                                                    error!("Error commiting transaction. PUT {}", &path);
+                                                    response.set_status(StatusCode::InternalServerError);
+                                                }
+                                            };
+                                        },
+                                        Err(_) => {
+                                            error!("Could not put into database");
+                                            response.set_status(StatusCode::InternalServerError);
+                                        }
+                                    };
+                                },
+                                Err(_) => {
+                                    error!("Trouble opening database rw transaction");
+                                    response.set_status(StatusCode::InternalServerError);
+                                }
+                            };
+                            Ok(response)
+                        }).boxed()
+                    },
+                    Method::Delete => {
+                        futures::future::ok(1).and_then( move |_: u32| {
+                            let txn = DATABASE.begin_rw_txn();
+                            match txn {
+                                Ok(mut txn) => {
+                                    let value = txn.del(db_handle, &path, None);
+                                    match value {
+                                        Ok(_) => {
+                                            match txn.commit() {
+                                                Ok(_) => {
+                                                    info!("Deleted entry from database.");
+                                                    response.set_status(StatusCode::Found);
+                                                },
+                                                Err(_) => {
+                                                    error!("Could not find entry to delete.");
+                                                    response.set_status(StatusCode::NotFound);
+                                                }
+                                            };
+                                        },
+                                        Err(_) => {
+                                            error!("Could not find entry to delete.");
+                                            response.set_status(StatusCode::NotFound);
+                                        }
+                                    };
+                                },
+                                Err(_) => {
+                                    error!("Could not get read/write transaction");
+                                    response.set_status(StatusCode::InternalServerError);
+                                }
+                            };
+                            Ok(response)
+                        }).boxed()
+                    },
+                    Method::Options => {
+                        futures::future::ok(1).and_then( move |_: u32| {
+                            let txn = DATABASE.begin_ro_txn();
+                            match txn {
+                                Ok(txn) => {
+                                    let value = txn.get(db_handle, &path);
+                                    match value {
+                                        Ok(value) => {
+                                            debug!("OPTIONS requested for {}", path);
+                                            let body = String::from_utf8(value.to_vec()).unwrap();
+                                            response.set_body(body);
+                                        },
+                                        Err(_) => {
+                                            error!("Value not found (OPTIONS)");
+                                            response.set_status(StatusCode::NotFound);
+                                        }
+
+                                    }
+                                },
+                                Err(_) => {
+                                    error!("Could not open database transaction.");
+                                    response.set_status(StatusCode::InternalServerError);
+                                }
+                            };
+                            Ok(response)
+                        }).boxed()
+                    },
+                    _ => {
+                        error!("Received bad request.");
+                        response.set_status(StatusCode::BadRequest);
+                        futures::future::ok(response).boxed()
+                    },
+                }
             },
-            Method::Put => {
-                body.concat2().and_then(move |body_c: Chunk| {
-                    let mut txn = DATABASE.begin_rw_txn().unwrap();
-                    txn.put(db_handle, &path, &body_c, WriteFlags::empty()).unwrap();
-                    txn.commit().unwrap();
-                    Ok(response)
-                }).boxed()
-            },
-            Method::Options => {
-                futures::future::ok(1).and_then( move |_: u32| {
-                    let txn = DATABASE.begin_ro_txn().unwrap();
-                    let value = txn.get(db_handle, &path).unwrap();
-                    let body = String::from_utf8(value.to_vec()).unwrap();
-                    response.set_body(body);
-                    Ok(response)
-                }).boxed()
-            },
-            _ => {
-                response.set_status(StatusCode::BadRequest);
+            Err(_) => {
+                response.set_status(StatusCode::InternalServerError);
                 futures::future::ok(response).boxed()
-            },
+            }
         }
     }
 }
 
 fn main() {
+    pretty_env_logger::init().unwrap();
     let addr = "0.0.0.0:3000".parse().unwrap();
     let server = Http::new().bind(&addr, move || Ok(GoLinkServer)).unwrap();
     server.run().unwrap();
